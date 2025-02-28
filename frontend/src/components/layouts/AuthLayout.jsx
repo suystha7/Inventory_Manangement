04e12057902e6294e59c38a1cb7d3bf2171d6527
suyogstha7 <suyogstha317@gmail.com>
2025-02-28 15:10:59 +0545

    backend

--- frontend/src/components/layouts/AuthLayout.jsx
+++ frontend/src/components/layouts/AuthLayout.jsx
@@ -4,12 +4,7 @@ import { LuTrendingUpDown } from "react-icons/lu";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="flex">
-      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className="text-3xl font-medium text-black">Expense Tracker</h2>
-        {children}
-      </div>
-
+    <div className="flex overflow-hidden">
       <div className="hidden md:block w-[50vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative">
         <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5" />
         <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-[10%]" />
@@ -28,6 +23,11 @@ const AuthLayout = ({ children }) => {
           src={Card_1}
           className="w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15"
         />
+      </div>
+
+      <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+        <h2 className="text-3xl font-medium text-black">Expense Tracker</h2>
+        {children}
       </div>
     </div>
   );
